1fd20c16f444da9630e78121072fb0f4558f4ed8
Kirill <youngfave@yandex.ru>
2024-10-08 20:14:39 +0700

    Init OrderService

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Database;
+using OrderService.Models;
+using OrderService.Repositories;
+
+namespace OrderService.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class OrderController : Controller
+{
+    private readonly IRepository<Order> _orderRepository;
+
+    public OrderController(IRepository<Order> orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> Get(Guid guid)
+    {
+        Order? order = await _orderRepository.GetByIdAsync(guid);
+
+        return order is null ? NotFound() : Ok(order);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Create(Order order)
+    {
+        bool result = await _orderRepository.AddAsync(order);
+
+        return result ? NoContent() : Created();
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult> Update(Order order)
+    {
+        OperationResult result = await _orderRepository.UpdateAsync(order);
+
+        return result switch
+        {
+            OperationResult.NotEntityFound => NotFound(),
+            OperationResult.Success => NoContent(),
+            OperationResult.NotChangesApplied => StatusCode(500, $"Failed to save the {nameof(Order)} to the database."),
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> Delete(Guid guid)
+    {
+        OperationResult result = await _orderRepository.DeleteAsync(guid);
+
+        return result switch
+        {
+            OperationResult.NotEntityFound => NotFound(),
+            OperationResult.Success => NoContent(),
+            OperationResult.NotChangesApplied => StatusCode(500, $"Failed to delete the {nameof(Order)} to the database."),
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
---
+++ OrderService/Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using OrderService.Dto;
+using OrderService.Models;
+
+using AutoMapper;
+namespace OrderService.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Order, CreateOrderDto>();
+    }
+}
