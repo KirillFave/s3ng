24d2baa3a953c691c21a6f7fad3c395cd38d6bb6
Kirill <youngfave@yandex.ru>
2024-11-03 01:40:32 +0700

    index on Init-OrderService: 6f86285 Init OrderService

---
+++ OrderService/Dto/UpdateOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using OrderService.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderService.Dto;
+
+public class UpdateOrderDto
+{
+    public required Guid Guid { get; set; }
+    public PaymentType? PaymentType { get; set; }
+
+    [MaxLength(255)]
+    public string? ShipAddress { get; set; }
+}
---
+++ OrderService/Migrations/20241029182454_Init.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserGuid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    PaymentType = table.Column<string>(type: "TEXT", nullable: false),
+                    ShipAddress = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    CreatedTimestamp = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    OrderGuid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    ProductGuid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PricePerUnit = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Count = table.Column<int>(type: "INTEGER", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderGuid",
+                        column: x => x.OrderGuid,
+                        principalTable: "Orders",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderGuid",
+                table: "OrderItems",
+                column: "OrderGuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
