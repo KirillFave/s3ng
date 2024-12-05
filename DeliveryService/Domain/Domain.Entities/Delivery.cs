b4096e3fea62eb1cd26645d059ef0e83093ad642
Dmitrypo <mitrybox@gmail.com>
2024-12-05 10:20:21 +0300

    Delivery.cs fields list updated

--- DeliveryService/Domain/Domain.Entities/Delivery.cs
+++ DeliveryService/Domain/Domain.Entities/Delivery.cs
@@ -1,6 +1,19 @@
-﻿namespace DeliveryService.Domain.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeliveryService.Domain.Domain.Entities
 {
-    public class Delivery
+    public class Delivery : IEntity<Guid>
     {
+        [Key]
+        public required Guid Id { get; set; }
+        public required string OrderStatus { get; set; }
+        public decimal Total_Price { get; set; }
+        
+        [MaxLength(200)]
+        public required string Shipping_Address { get; set; }
+        //public PaymentType PaymentType { get; set; }
+        public int Order_Id { get; set; }
+        public int Courer_Id { get; set; }
+        public DateTime Delivery_Time { get; set; }
     }
 }
