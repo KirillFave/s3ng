cdbff2fa8eb8344f99b49317141a2823dbb7f096
Dmitry Ponomar <mitrybox@gmail.com>
2024-12-06 21:40:33 +0300

    DeliveryDBContext added

--- src/Services/DeliveryService/Data/DeliveryContext.cs
+++ src/Services/DeliveryService/Data/DeliveryContext.cs
@@ -1,7 +1,13 @@
-﻿namespace DeliveryService.Data
+﻿using Microsoft.EntityFrameworkCore;
+using DeliveryService.Domain.Domain.Entities;
+
+namespace DeliveryService.Data
 {
     public class DeliveryDBContext : DbContext
     {  
-       
+       public DbSet<DeliveryModel> Deliveries {  get; set; }
+        public DeliveryDBContext(DbContextOptions<DeliveryDBContext> options) : base(options) 
+        {
+        }
     }
 }
--- src/Services/DeliveryService/Domain/Domain.Entities/DeliveryModel.cs
+++ src/Services/DeliveryService/Domain/Domain.Entities/DeliveryModel.cs
@@ -2,12 +2,13 @@
 
 namespace DeliveryService.Domain.Domain.Entities
 {
-    public class Delivery : IEntity<Guid>
+    public class DeliveryModel : IEntity<Guid>
     {
         [Key]
         public required Guid Id { get; set; }
         public required string OrderStatus { get; set; }
-        public decimal Total_Price { get; set; }
+        public required decimal Total_Price { get; set; }
+        public required decimal Total_Quantity { get; set; }
         
         [MaxLength(200)]
         public required string Shipping_Address { get; set; }
