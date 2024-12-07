f81fb28b6097a9781b94f8b5b52b59b0c7d7ac3e
Dmitry Ponomar <mitrybox@gmail.com>
2024-12-07 07:00:55 +0300

    DeliveryRepository fix Getting all types that implement the IDeliveryRepository

--- src/Services/DeliveryService/Repositories/DeliveryRepository.cs
+++ src/Services/DeliveryService/Repositories/DeliveryRepository.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using DeliveryService.Data;
+using DeliveryService.Domain.Domain.Entities;
 
 namespace DeliveryService.Repositories
 {
@@ -9,26 +12,16 @@ namespace DeliveryService.Repositories
     /// <typeparam name="TPrimaryKey"> Тип первичного ключа. </typeparam>
     public class DeliveryRepository : IDeliveryRepository
     {
-        protected readonly DbContext Context;
-        private readonly DbSet<T> _entitySet;
+        protected readonly DeliveryDBContext Context;
+        private readonly DbSet<Delivery> _entityDeliverySet;
 
-        protected Repository(DbContext context)
+        public DeliveryRepository(DeliveryDBContext context)
         {
             Context = context;
-            _entitySet = Context.Set<T>();
+            _entityDeliverySet = Context.Set<Delivery>();
         }
 
-        #region Get
-
-        /// <summary>
-        /// Получить сущность по ID.
-        /// </summary>
-        /// <param name="id"> Id сущности. </param>
-        /// <returns> Cущность. </returns>
-        public virtual T Get(TPrimaryKey id)
-        {
-            return _entitySet.Find(id);
-        }
+        #region Get     
 
         /// <summary>
         /// Получить сущность по Id.
@@ -36,9 +29,20 @@ namespace DeliveryService.Repositories
         /// <param name="id"> Id сущности. </param>
         /// <param name="cancellationToken"></param>
         /// <returns> Cущность. </returns>
-        public virtual async Task<T> GetAsync(TPrimaryKey id, CancellationToken cancellationToken)
+        public async Task<Delivery> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            return await _entitySet.FindAsync((object)id);
+            return await _entityDeliverySet.FindAsync(id, cancellationToken);
+        }
+
+        /// <summary>
+        /// Получить сущность по AuthenticationId.
+        /// </summary>
+        /// <param name="id"> Id сущности. </param>
+        /// <param name="cancellationToken"></param>
+        /// <returns> Cущность. </returns>
+        public async Task<Delivery> GetByAuthenticationIdAsync(Guid authenticationId, CancellationToken cancellationToken)
+        {
+            return await _entityDeliverySet.FindAsync(authenticationId, cancellationToken);
         }
 
         #endregion
@@ -48,70 +52,48 @@ namespace DeliveryService.Repositories
         /// <summary>
         /// Запросить все сущности в базе.
         /// </summary>
-        /// <param name="asNoTracking"> Вызвать с AsNoTracking. </param>
-        /// <returns> IQueryable массив сущностей. </returns>
-        public virtual IQueryable<T> GetAll(bool asNoTracking = false)
-        {
-            return asNoTracking ? _entitySet.AsNoTracking() : _entitySet;
-        }
-
-        /// <summary>
-        /// Запросить все сущности в базе.
-        /// </summary>
         /// <param name="cancellationToken"> Токен отмены </param>
-        /// <param name="asNoTracking"> Вызвать с AsNoTracking. </param>
         /// <returns> Список сущностей. </returns>
-        public async Task<List<T>> GetAllAsync(CancellationToken cancellationToken, bool asNoTracking = false)
+        public async Task<List<Delivery>> GetAllAsync(CancellationToken cancellationToken)
         {
-            return await GetAll().ToListAsync(cancellationToken);
+            return await _entityDeliverySet.ToListAsync(cancellationToken);
         }
 
         #endregion
 
-        #region Create
-
-        /// <summary>
-        /// Добавить в базу сущность.
-        /// </summary>
-        /// <param name="entity"> Cущность для добавления. </param>
-        /// <returns>. Добавленная сущность. </returns>
-        public virtual T Add(T entity)
-        {
-            var objToReturn = _entitySet.Add(entity);
-            return objToReturn.Entity;
-        }
-
+        #region Create 
+        
         /// <summary>
         /// Добавить в базу одну сущность.
         /// </summary>
         /// <param name="entity"> Сущность для добавления. </param>
         /// <returns> Добавленная сущность. </returns>
-        public virtual async Task<T> AddAsync(T entity)
+        public async Task<Delivery> AddAsync(Delivery entity, CancellationToken cancellationToken)
         {
-            return (await _entitySet.AddAsync(entity)).Entity;
+            return (await _entityDeliverySet.AddAsync(entity, cancellationToken)).Entity;
         }
 
         /// <summary>
         /// Добавить в базу массив сущностей.
         /// </summary>
         /// <param name="entities"> Массив сущностей. </param>
-        public virtual void AddRange(List<T> entities)
+        public virtual void AddRange(List<Delivery> entities)
         {
-            var enumerable = entities as IList<T> ?? entities.ToList();
-            _entitySet.AddRange(enumerable);
+            var enumerable = entities as IList<Delivery> ?? entities.ToList();
+            _entityDeliverySet.AddRange(enumerable);
         }
 
         /// <summary>
         /// Добавить в базу массив сущностей.
         /// </summary>
         /// <param name="entities"> Массив сущностей. </param>
-        public virtual async Task AddRangeAsync(ICollection<T> entities)
+        public virtual async Task AddRangeAsync(ICollection<Delivery> entities)
         {
             if (entities == null || !entities.Any())
             {
                 return;
             }
-            await _entitySet.AddRangeAsync(entities);
+            await _entityDeliverySet.AddRangeAsync(entities);
         }
 
         #endregion
@@ -122,9 +104,20 @@ namespace DeliveryService.Repositories
         /// Для сущности проставить состояние - что она изменена.
         /// </summary>
         /// <param name="entity"> Сущность для изменения. </param>
-        public virtual void Update(T entity)
+        public virtual void Update(Delivery entity)
         {
             Context.Entry(entity).State = EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Обновить в базе сущность.
+        /// </summary>
+        /// <param name="entity"> Сущность для обновления. </param>
+        /// <returns> Обновленная сущность. </returns>
+        public async Task<Delivery> UpdateAsync(Delivery entity, CancellationToken cancellationToken)
+        {
+            _entityDeliverySet.Update(entity);
+            return await _entityDeliverySet.FindAsync(entity.Id, cancellationToken);
         }
 
         #endregion
@@ -134,16 +127,17 @@ namespace DeliveryService.Repositories
         /// <summary>
         /// Удалить сущность.
         /// </summary>
-        /// <param name="id"> Id удалённой сущности. </param>
+        /// <param name="id"> Id удаляемой сущности. </param>
         /// <returns> Была ли сущность удалена. </returns>
-        public virtual bool Delete(TPrimaryKey id)
+        public async Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken)
         {
-            var obj = _entitySet.Find(id);
-            if (obj == null)
+            var deleteEntity = await _entityDeliverySet.FindAsync(id, cancellationToken);
+            if (deleteEntity == null)
             {
                 return false;
             }
-            _entitySet.Remove(obj);
+
+            _entityDeliverySet.Remove(deleteEntity);
             return true;
         }
 
@@ -152,7 +146,7 @@ namespace DeliveryService.Repositories
         /// </summary>
         /// <param name="entity"> Сущность для удаления. </param>
         /// <returns> Была ли сущность удалена. </returns>
-        public virtual bool Delete(T entity)
+        public virtual bool Delete(Delivery entity)
         {
             if (entity == null)
             {
@@ -167,13 +161,13 @@ namespace DeliveryService.Repositories
         /// </summary>
         /// <param name="entities"> Коллекция сущностей для удаления. </param>
         /// <returns> Была ли операция завершена успешно. </returns>
-        public virtual bool DeleteRange(ICollection<T> entities)
+        public virtual bool DeleteRange(ICollection<Delivery> entities)
         {
             if (entities == null || !entities.Any())
             {
                 return false;
             }
-            _entitySet.RemoveRange(entities);
+            _entityDeliverySet.RemoveRange(entities);
             return true;
         }
 
@@ -184,15 +178,7 @@ namespace DeliveryService.Repositories
         /// <summary>
         /// Сохранить изменения.
         /// </summary>
-        public virtual void SaveChanges()
-        {
-            Context.SaveChanges();
-        }
-
-        /// <summary>
-        /// Сохранить изменения.
-        /// </summary>
-        public virtual async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             await Context.SaveChangesAsync(cancellationToken);
         }
