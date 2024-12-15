db68131e446c690ff93e13392c67f5ef9a950d59
Kirill <youngfave@yandex.ru>
2024-12-15 12:51:38 +0700

    feat [OrderService]: WebHost controllers + docker file

---
+++ src/ApiGateway/WebHost/OrderServiceConfiguration/OrderServiceConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using OrderService.Database;
+using OrderService.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace WebHost.OrderServiceConfiguration;
+
+public static class OrderServiceConfiguration
+{
+    public static IServiceCollection ConfigureOrderService(this IServiceCollection services, 
+                                                           IConfiguration configuration)
+    {
+        services.AddScoped(typeof(OrderRepository));
+        services.AddScoped(typeof(OrderItemRepository));
+
+        services.AddDbContext<DatabaseContext>(
+            optionsBuilder => optionsBuilder.UseSqlite(configuration["OrderService.ConnectionString"]));
+
+        return services;
+    }
+}
