794fb47a5d114e19cd09d639fcd236c945879051
ivar <ivar@deeplay.io>
2024-12-19 01:11:13 +0700

    доработки контроллера юзеров

--- src/ApiGateway/WebHost/Controllers/UserController.cs
+++ src/ApiGateway/WebHost/Controllers/UserController.cs
@@ -2,7 +2,9 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WebHost.Dto.User;
 using static CreateUserResponse.Types;
+using static DeleteUserResponse.Types;
 using static GetUserResponse.Types;
+using static UpdateUserResponse.Types;
 using ILogger = Serilog.ILogger;
 
 namespace WebHost.Controllers;
@@ -25,12 +27,12 @@ public class UserController : ControllerBase
     /// <summary>
     /// Получение юзера по ID
     /// </summary>
-    [HttpGet("{id}")]
+    [HttpGet("get{id}")]
     [ProducesResponseType<UserDto>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetUserAsync(string id)
     {
-        _logger.Information($"Api method {nameof(GetUserAsync)} was called with parameters {id}");
+        _logger.Information($"Api method GetUserAsync was called with parameters {id}");
         var request = new GetUserByIdRequest { Id = id };
 
         var result = await _client.GetUserByIdAsync(request);
@@ -40,7 +42,7 @@ public class UserController : ControllerBase
             return new BadRequestObjectResult(result.Result);
 
         var userInfo = _mapper.Map<UserDto>(result.User);
-        return new OkObjectResult(result.User);
+        return new OkObjectResult(userInfo);
     }
 
     /// <summary>
@@ -51,7 +53,7 @@ public class UserController : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> CreateUserAsync(CreateUserRequestDto request)
     {
-        _logger.Information($"Api method {nameof(CreateUserAsync)} was called with name: {request.FirstName}, {request.LastName}");
+        _logger.Information($"Api method CreateUserAsync was called with name: {request.FirstName}, {request.LastName}");
         var createUserRequest = _mapper.Map<CreateUserRequest>(request);
         var result = await _client.CreateUserAsync(createUserRequest);
 
@@ -61,5 +63,46 @@ public class UserController : ControllerBase
             return new BadRequestObjectResult(result.Result);
 
         return new OkObjectResult(result.User.Id);
+    }
+
+    /// <summary>
+    /// Редактирование информации о юзере
+    /// </summary>
+    [HttpPut("edit")]
+    [ProducesResponseType<UserDto>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UpdateUserAsync(UpdateUserRequestDto request)
+    {
+        _logger.Information($"Api method UpdateUserAsync was called");
+        var updateUserRequest = _mapper.Map<UpdateUserRequest>(request);
+        var result = await _client.UpdateUserAsync(updateUserRequest);
+
+        _logger.Information($"end call UpdateUserAsync with result {result.Result}");
+
+        if (result.Result != UpdateUserResult.Success)
+            return new BadRequestObjectResult(result.Result);
+
+        var userInfo = _mapper.Map<UserDto>(result.User);
+        return new OkObjectResult(userInfo);
+    }
+
+    /// <summary>
+    /// Удаление юзера
+    /// </summary>
+    [HttpDelete("delete{id}")]
+    [ProducesResponseType<bool>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DeleteUserAsync(string id)
+    {
+        _logger.Information($"Api method DeleteUserAsync was called with parameters {id}");
+        var request = new DeleteUserRequest { Id = id };
+
+        var result = await _client.DeleteUserAsync(request);
+        _logger.Information($"end call DeleteUserAsync with result {result.Result}");
+
+        if (result.Result != DeleteUserResult.Success)
+            return new BadRequestObjectResult(result.Result);
+
+        return new OkObjectResult(true);
     }
 }
--- src/ApiGateway/WebHost/Dto/User/UserServiceDto.cs
+++ src/ApiGateway/WebHost/Dto/User/UserServiceDto.cs
@@ -1,6 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 namespace WebHost.Dto.User;
 
 public class CreateUserRequestDto
@@ -11,12 +8,6 @@ public class CreateUserRequestDto
     public long Phone { get; set; }
     public required string Address { get; set; }
     public required RoleDto Role { get; set; }
-}
-
-public class CreateUserResponseDto
-{
-    public UserDto User { get; set; }
-    public UserResultDto Result { get; set; }
 }
 
 public enum UserResultDto
@@ -47,28 +38,11 @@ public class UserDto
     public required RoleDto Role { get; set; }
 }
 
-public class DeleteUserResponseDto
-{
-    public UserResultDto Result { get; set; }
-}
-
-public class GetUserResponseDto
-{
-    public UserDto User { get; set; }
-    public UserResultDto Result { get; set; }
-}
-
 public class UpdateUserRequestDto
 {
-    public required Guid Id { get; set; }
+    public required string Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Address { get; set; }
     public long Phone { get; set; }
-}
-
-public class UpdateUserResponseDto
-{
-    public UserDto User { get; set; }
-    public UserResultDto Result { get; set; }
 }
