3f89fd178935f7eecd459340759655032bbf2c3a
Dmitry Ponomar <mitrybox@gmail.com>
2024-12-19 10:07:41 +0300

    IDeliveryRepository corrected

--- src/Services/DeliveryService/Repositories/IDeliveryRepository.cs
+++ src/Services/DeliveryService/Repositories/IDeliveryRepository.cs
@@ -3,13 +3,12 @@ using DeliveryService.Domain.Domain.Entities;
 
 namespace DeliveryService.Repositories;
 
-    public interface IDeliveryRepository<T>
-    where T : 
+    public interface IDeliveryRepository    
 {
     /// <summary>
     /// Запросить все сущности в базе.
     /// </summary>
-    /// <param name="cancellationToken"> Токен отмены. </param>
+    /// <param name="cancellationToken"> Токен отмены </param>
     /// <returns> Список сущностей. </returns>
     Task<List<Delivery>> GetAllAsync(CancellationToken cancellationToken);
 
@@ -34,7 +33,7 @@ namespace DeliveryService.Repositories;
     /// </summary>
     /// <param name="entity"> Сущность для добавления. </param>
     /// <returns> Добавленная сущность. </returns>
-    Task<Delivery> AddAsync(T entity);
+    Task<Delivery> AddAsync(Delivery entity);
 
     /// <summary>
     /// Удалить сущность.
@@ -54,8 +53,7 @@ namespace DeliveryService.Repositories;
     /// Сохранить изменения.
     /// </summary>
     Task SaveChangesAsync(CancellationToken cancellationToken = default);
-    Task<Domain.Domain.Entities.Delivery> GetByIdAsync(Guid guid);
-    Task<bool> AddAsync(Domain.Domain.Entities.Delivery delivery);
+    Task<Domain.Domain.Entities.Delivery> GetByIdAsync(Guid guid);    
     Task<OperationResults> UpdateAsync(Domain.Domain.Entities.Delivery delivery);
     Task<OperationResults> DeleteAsync(Guid guid);
 }
