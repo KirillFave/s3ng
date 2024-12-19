f356d1e8211cd9b13223dee8a2ceff7fbdd1f0c8
Dmitry Ponomar <mitrybox@gmail.com>
2024-12-19 14:42:22 +0300

    Models.OrderMessage created

--- src/Services/DeliveryService/Models/OrderMessage.cs
+++ src/Services/DeliveryService/Models/OrderMessage.cs
@@ -1,17 +1,18 @@
-﻿using DeliveryService.Domain.Domain.Entities;
+﻿using DeliveryService.Domain.External.Entities;
 using DeliveryService.Enums;
+using DeliveryService.Domain.External;
 
-namespace DeliveryService.Domain.External.Entities
+namespace DeliveryService.Models
 {
-    public class Order : IEntity<Guid>
+    public class OrderMessage
     {
-        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
         public List<OrderItem> Items { get; set; }
         public int Total_Quantity { get; set; }
         public decimal Total_Price { get; set; }
         public OrderStatus Status { get; set; }
         public PaymentType PaymentType { get; set; }
-        public required string Shipping_Address { get; set; }
+        public string Shipping_Address { get; set; }
         public DateTime CreatedTimestamp { get; set; }
     }
 }
--- src/Services/DeliveryService/Program.cs
+++ src/Services/DeliveryService/Program.cs
@@ -3,6 +3,7 @@ using DeliveryService.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+//using OrderService;
 
             var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,15 @@ using Microsoft.Extensions.Configuration;
                 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
-            var app = builder.Build();
+//var apiProductService = builder.AddProject<Projects.DeliveryService>("apiservice-delivery");
+
+//var apiOrderService = builder.AddProject<Projects.OrderSErvice>("apiservice-order");
+
+//builder.AddProject<Projects.ApiGateway("webfrontend")
+//    .WithReference(apiDeliveryService)
+//    .WithReference(apiOrderService);
+
+var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
