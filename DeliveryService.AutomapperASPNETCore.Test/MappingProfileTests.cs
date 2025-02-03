860fd47fc000763938c5a0b4a54347c3c29b352c
Dmitry Ponomar <mitrybox@gmail.com>
2025-02-03 21:23:15 +0300

    MappingProfileTests added

---
+++ DeliveryService.AutomapperASPNETCore.Test/MappingProfileTests.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using OrderService.Mapping;  //?
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryService.AutomapperASPNETCore.Test
+{
+    public class MappingProfileTests
+    {
+        [Fact]
+        public void ValidateMappingConfigurationTest()
+        {
+            MapperConfiguration mapperConfig = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = new Mapper(mapperConfig);
+
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
