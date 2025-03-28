26b14e219d101909d1e1f616fb224e3e2902924b
Dmitry Ponomar <mitrybox@gmail.com>
2025-03-28 18:47:38 +0300

    Kafka BaseConsumer corrected

--- src/Services/DeliveryService/DeliveryService/DeliveryService/Kafka/Consumers/BaseConsumer.cs
+++ src/Services/DeliveryService/DeliveryService/DeliveryService/Kafka/Consumers/BaseConsumer.cs
@@ -4,73 +4,34 @@ using DeliveryService.Kafka.Models;
 using DeliveryService.Kafka.Options;
 using DeliveryService.Kafka.Utils;
 using Microsoft.Extensions.Logging;
-using ILogger = Serilog.ILogger;
+//using ILogger = Serilog.ILogger;
 
 namespace DeliveryService.Kafka.Consumers;
 public sealed class BaseConsumer<TKey, TValue> where TValue : IKafkaMessage
 {
     private ILogger _logger;
-    private KafkaOptions _kafkaOptions;
+
     public IConsumer<TKey, TValue> Consumer { get; }
 
     public BaseConsumer(KafkaOptions kafkaOptions, ILogger logger, string groupId)
     {
         _logger = logger;
-        _kafkaOptions = kafkaOptions;
-
-        try
+        var consumerConfig = new ConsumerConfig
         {
-            var consumerConfig = new ConsumerConfig
+            GroupId = groupId,
+            BootstrapServers = kafkaOptions.BootstrapServers,
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = false,
+        };
+        ConsumerBuilder<TKey, TValue> consumerBuilder = new ConsumerBuilder<TKey, TValue>(consumerConfig);
+        Consumer = consumerBuilder
+            .SetErrorHandler((_, error) => _logger.LogError(error.Reason))
+            .SetLogHandler((_, message) =>
             {
-                GroupId = groupId,
-                BootstrapServers = _kafkaOptions.BootstrapServers,
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit = false,
-            };
-            ConsumerBuilder<TKey, TValue> consumerBuilder = new ConsumerBuilder<TKey, TValue>(consumerConfig);
-            Consumer = consumerBuilder
-                .SetErrorHandler((_, error) => _logger.Error(error.Reason))
-                .SetLogHandler((_, message) =>
-                {
-                    _logger.Information(message.Message);
-                })
-                .SetKeyDeserializer((IDeserializer<TKey>)Deserializers.Utf8)
-                .SetValueDeserializer(new KafkaMessageDeserializer<TValue>())
-                .Build();
-        }
-        catch (Exception ex)
-        {
-            _logger.Error($"BaseConsumer error {ex}");
-            throw;
-        }
-    }
-    public async Task EnsureTopicExists(string topicName)
-    {
-        var adminConfig = new AdminClientConfig { BootstrapServers = _kafkaOptions.BootstrapServers };
-
-        using var adminClient = new AdminClientBuilder(adminConfig).Build();
-
-        try
-        {
-            var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(5));
-
-            if (metadata.Topics.Any(t => t.Topic == topicName && !t.Error.IsError))
-            {
-                _logger.Information($"Topic '{topicName}' already exists");
-                return;
-            }
-
-            var topicSpecification = new TopicSpecification
-            {
-                Name = topicName
-            };
-
-            await adminClient.CreateTopicsAsync(new[] { topicSpecification });
-            _logger.Information($"Topic '{topicName}' successfully created");
-        }
-        catch (Exception ex)
-        {
-            _logger.Error($"Error for creation '{topicName}': {ex}");
-        }
+                _logger.LogInformation(message.Message);
+            })
+            .SetKeyDeserializer((IDeserializer<TKey>)Deserializers.Utf8)
+            .SetValueDeserializer(new KafkaMessageDeserializer<TValue>())
+            .Build();
     }
 }
